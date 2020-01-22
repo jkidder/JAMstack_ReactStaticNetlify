5672022d8c64caefc305f296770e3c79b2efebac
jonathan.kidder@gmail.com <jonathan.kidder@gmail.com>
2020-01-21 19:15:45 -0500

    first commit

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,27 @@
+// Imports
+import plugin0 from '/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/node_modules/react-static-plugin-reach-router/browser.api.js'
+
+// Plugins
+const plugins = [{
+        location: "/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/node_modules/react-static-plugin-source-filesystem",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/node_modules/react-static-plugin-sitemap/dist",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/src/pages/404.js': require('/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/src/pages/404.js').default,
+'/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/src/pages/about.js': require('/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/src/pages/about.js').default,
+'/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/src/pages/blog.js': require('/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/src/pages/blog.js').default,
+'/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/src/pages/index.js': require('/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/src/pages/index.js').default,
+'/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/src/containers/Post': require('/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/home/pi/Documents/reactstatic/JAMstack_ReactStaticNetlify/src/pages/404.js'
+
